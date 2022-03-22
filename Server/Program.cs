07bbf923831954d5f4c2b13703ac82043fe82250
mmporong <74720422+mmporong@users.noreply.github.com>
2022-03-22 10:11:24 +0900

    Cache

---
+++ Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10000, 10000];
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        arr[y, x] = 1;
+                    }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
+            }
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        arr[x, y] = 1;
+                    }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
+            }
+        }
+    }
+}
