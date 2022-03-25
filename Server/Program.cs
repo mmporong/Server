6a4d4403fc73bba746d9f213aa2e945e2f27f271
mmporong <74720422+mmporong@users.noreply.github.com>
2022-03-25 11:09:11 +0900

    Memory barrier / Interlocked

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,33 +4,34 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    // 메모리 배리어
+    // 코드 재배치 억제
+    // 가시성
     class Program
     {
+        static int number = 0;
+
+        static void Thread_1()
+        {
+            Interlocked.Increment(ref number);
+        }
+
+        static void Thread_2()
+        {
+            Interlocked.Decrement(ref number);
+        }
+
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                    for (int x = 0; x < 10000; x++)
-                    {
-                        arr[y, x] = 1;
-                    }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
-            }
+            Task.WaitAll(t1, t2);
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                    for (int x = 0; x < 10000; x++)
-                    {
-                        arr[x, y] = 1;
-                    }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
-            }
+            Console.WriteLine(number);
         }
     }
 }
+
